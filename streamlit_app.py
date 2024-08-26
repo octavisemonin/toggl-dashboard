7818bef6db6dd7aafdce00fa89655237a109ca4a
Octavi Semonin <octavi@gmail.com>
2024-08-26 17:45:19 +0000

    cleanup and black text

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ def hovertext(row):
     return '<br>'.join([f,h,r,e])
 
 @st.cache_data(ttl='1d')
-def get_gdp_data():
+def get_toggl_data():
     """Grab project data from Toggl.
 
     This uses caching to avoid having to read the file every time. If we were
@@ -97,43 +97,9 @@ def get_gdp_data():
     projects['hover_text'] = projects.apply(hovertext, axis=1)
     projects['color'] = [colors[i % num_colors] for i in range(len(projects))]
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    return projects
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df,projects
-
-gdp_df,projects = get_gdp_data()
+projects = get_toggl_data()
 
 # -----------------------------------------------------------------------------
 # Draw the actual page
@@ -164,6 +130,7 @@ fig.add_trace(go.Bar(
 
 # Update layout
 fig.update_layout(
+    font_color='black',
     margin=dict(l=20, r=20, t=20, b=20),
     height=600,  # Set the height of the figure to be taller
     width=800,   # Set the width of the figure to be narrower
@@ -205,6 +172,7 @@ actives_fig.add_trace(go.Bar(
 
 # Update layout
 actives_fig.update_layout(
+    font_color='black',
     margin=dict(l=20, r=20, t=20, b=20),
     height=600,  # Set the height of the figure to be taller
     width=800,   # Set the width of the figure to be narrower
